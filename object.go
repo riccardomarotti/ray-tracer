67a530946b16d9c040f302060a649993c0b8f4cb
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-06 19:45:38 +0100

    Add Hit

--- object.go
+++ object.go
@@ -6,3 +6,15 @@ type Intersection struct {
 	t      float64
 	object Object
 }
+
+func Hit(i []Intersection) (hit Intersection) {
+	hit = Intersection{}
+
+	for k := 0; k < len(i); k++ {
+		if i[k].t >= 0 && (i[k].t <= hit.t || hit.t == 0) {
+			hit = i[k]
+		}
+	}
+
+	return
+}
--- object_test.go
+++ object_test.go
@@ -19,3 +19,42 @@ func TestAggregatingIntersections(t *testing.T) {
 	AssertEqual(1, xs[0].t, t)
 	AssertEqual(2, xs[1].t, t)
 }
+
+func TestHitWhenAllIntersectionsHavePositiveT(t *testing.T) {
+	s := Sphere{}
+	i1 := Intersection{1, s}
+	i2 := Intersection{2, s}
+	xs := []Intersection{i2, i1}
+
+	Assert(i1 == Hit(xs), "", t)
+}
+
+func TestHitWhenSomeIntersectionsHaveNegativeT(t *testing.T) {
+	s := Sphere{}
+	i1 := Intersection{-1, s}
+	i2 := Intersection{1, s}
+	xs := []Intersection{i2, i1}
+
+	Assert(i2 == Hit(xs), "", t)
+}
+
+func TestHitWhenAllIntersectionsHaveNegativeT(t *testing.T) {
+	s := Sphere{}
+	i1 := Intersection{-1, s}
+	i2 := Intersection{-2, s}
+	xs := []Intersection{i2, i1}
+
+	Assert(Intersection{} == Hit(xs), "", t)
+}
+
+func TestHitIsAlwaysTheLowestNonNegativeIntersection(t *testing.T) {
+	s := Sphere{}
+	i1 := Intersection{5, s}
+	i2 := Intersection{7, s}
+	i3 := Intersection{-3, s}
+	i4 := Intersection{2, s}
+	xs := []Intersection{i1, i2, i3, i4}
+
+	Assert(i4 == Hit(xs), "", t)
+
+}

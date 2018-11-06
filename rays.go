cf8298e08151dd1b8cc19a42a9992727000cacb8
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-06 10:17:51 +0100

    Add Ray position

--- rays.go
+++ rays.go
@@ -3,3 +3,7 @@ package main
 type Ray struct {
 	origin, direction Tuple
 }
+
+func (r Ray) Position(d float64) Tuple {
+	return r.direction.Multiply(d).Add(r.origin)
+}
--- rays_test.go
+++ rays_test.go
@@ -11,3 +11,12 @@ func TestCreatingAndQueryingARay(t *testing.T) {
 	AssertTupleEqual(origin, r.origin, t)
 	AssertTupleEqual(direction, r.direction, t)
 }
+
+func TestComputeAPointFromADistanceAlongTheRay(t *testing.T) {
+	r := Ray{Point(2, 3, 4), Vector(1, 0, 0)}
+
+	AssertTupleEqual(Point(2, 3, 4), r.Position(0), t)
+	AssertTupleEqual(Point(3, 3, 4), r.Position(1), t)
+	AssertTupleEqual(Point(1, 3, 4), r.Position(-1), t)
+	AssertTupleEqual(Point(4.5, 3, 4), r.Position(2.5), t)
+}

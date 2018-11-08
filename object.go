f856e27ef6d0dc5852eada2c6d19f7e811bdacbe
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-08 13:48:51 +0100

    Move Intersection struct to its own file

--- object.go
+++ object.go
@@ -5,20 +5,3 @@ type Object interface {
 	NormalAt(p Tuple) Tuple
 	Material() Material
 }
-
-type Intersection struct {
-	t      float64
-	object Object
-}
-
-func Hit(i []Intersection) (hit Intersection) {
-	hit = Intersection{}
-
-	for k := 0; k < len(i); k++ {
-		if i[k].t >= 0 && (i[k].t <= hit.t || hit.t == 0) {
-			hit = i[k]
-		}
-	}
-
-	return
-}

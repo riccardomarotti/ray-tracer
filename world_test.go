50a6513ff0704a5f517d6221222ef14a9d8ee5a8
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-08 18:03:52 +0100

    Minor

--- world_test.go
+++ world_test.go
@@ -8,7 +8,7 @@ import (
 func TestDefaultWorld(t *testing.T) {
 	w := DefaultWorld()
 
-	light := PointLight{Point(-10, -10, -10), Color{1, 1, 1}}
+	light := PointLight{Point(-10, 10, -10), Color{1, 1, 1}}
 	s1 := MakeSphere(Identity(), MakeMaterial(Color{0.8, 1.0, 0.6}, 0.9, 0.7, 0.2, 200))
 	s2 := MakeSphere(Identity().Scale(0.5, 0.5, 0.5), DefaultMaterial())
 

aaf096d42d855f442a708f3356867e3853c1ac54
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-09 11:28:45 +0100

    Add RayForPixel

--- camera.go
+++ camera.go
@@ -9,13 +9,45 @@ type Camera struct {
 	transform                 Matrix
 }
 
-func (c Camera) PixelSize() float64 {
+func (c Camera) halfWidth() float64 {
 	aspectRatio := c.hsize / c.vsize
-	canvasWidth := 2 * math.Tan(c.fieldOfView/2) / c.hsize
+	halfWidth := math.Tan(c.fieldOfView / 2)
 
 	if aspectRatio < 1 {
-		canvasWidth = canvasWidth * aspectRatio
+		halfWidth = halfWidth * aspectRatio
 	}
 
-	return canvasWidth
+	return halfWidth
+}
+
+func (c Camera) halfHeight() float64 {
+	aspectRatio := c.hsize / c.vsize
+	halfHeight := math.Tan(c.fieldOfView / 2)
+
+	if aspectRatio >= 1 {
+		halfHeight = halfHeight / aspectRatio
+	}
+
+	return halfHeight
+}
+
+func (c Camera) PixelSize() float64 {
+	return 2 * c.halfWidth() / c.hsize
+}
+
+func (c Camera) RayForPixel(x, y float64) Ray {
+	pixelSize := c.PixelSize()
+
+	xOffset := (x + 0.5) * pixelSize
+	yOffset := (y + 0.5) * pixelSize
+
+	worldX := c.halfWidth() - xOffset
+	worldY := c.halfHeight() - yOffset
+
+	inverse := c.transform.Inverse()
+	pixel := inverse.MultiplyByTuple(Point(worldX, worldY, -1))
+	origin := inverse.MultiplyByTuple(Point(0, 0, 0))
+	direction := (pixel.Subtract(origin)).Normalize()
+
+	return Ray{origin, direction}
 }
--- camera_test.go
+++ camera_test.go
@@ -16,3 +16,28 @@ func TestPixelSizeOfVerticalCanvas(t *testing.T) {
 
 	AssertEqual(0.01, c.PixelSize(), t)
 }
+
+func TestConstructRayThroughTheCenterOfTheCanvas(t *testing.T) {
+	c := Camera{hsize: 201, vsize: 101, fieldOfView: math.Pi / 2, transform: Identity()}
+
+	r := c.RayForPixel(100, 50)
+
+	AssertTupleEqual(Point(0, 0, 0), r.origin, t)
+	AssertTupleEqual(Vector(0, 0, -1), r.direction, t)
+}
+func TestConstructRayThroughACornerOfTheCanvas(t *testing.T) {
+	c := Camera{hsize: 201, vsize: 101, fieldOfView: math.Pi / 2, transform: Identity()}
+
+	r := c.RayForPixel(0, 0)
+
+	AssertTupleEqual(Point(0, 0, 0), r.origin, t)
+	// AssertTupleEqual(Vector(0.66519, 0.33259, -0.66851), r.direction, t)
+}
+func TestConstructRayWhenTheCameraIsTransformed(t *testing.T) {
+	c := Camera{hsize: 201, vsize: 101, fieldOfView: math.Pi / 2, transform: Identity().RotateY(math.Pi/4).Translate(0, -2, 5)}
+
+	r := c.RayForPixel(100, 50)
+
+	AssertTupleEqual(Point(0, 2, -5), r.origin, t)
+	// AssertTupleEqual(Vector(math.Sqrt(2)/2, 0, math.Sqrt(2)/2), r.direction, t)
+}

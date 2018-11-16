58f6387c4a2d035ef963b3756c6648070785cb00
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-16 10:50:45 +0100

    Fix build

--- plane_test.go
+++ plane_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestTheNormalOfAPlaneIsConstantEverywhere(t *testing.T) {
-	p := Plane{}
+	p := MakePlane(Identity(), DefaultMaterial())
 	n1 := p.NormalAt(Point(0, 0, 0))
 	n2 := p.NormalAt(Point(10, 0, -10))
 	n3 := p.NormalAt(Point(-5, 0, 150))

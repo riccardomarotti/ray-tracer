9b7d98e71bdf911956182a363bc3fc311632f212
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-21 21:12:13 +0100

    Update example image

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	camera := Camera{250, 125, math.Pi / 3, ViewTransform(Point(-1.3, 1.5, -5), Point(0, 1, 0), Vector(0, 1, 0))}
+	camera := Camera{1000, 500, math.Pi / 3, ViewTransform(Point(-1.3, 1.5, -5), Point(0, 1, 0), Vector(0, 1, 0))}
 	sphereMaterial := DefaultMaterial()
 	pattern1 := MakePerturbPattern(MakeRingPattern(Color{0.2, 1, 1}, Color{.8, 1, .8}, Identity().Scale(.05, .05, .05).RotateX(-math.Pi/3)))
 	pattern2 := MakeGradientPattern(Color{1, .5, .3}, Color{.3, .5, 1}, Identity().RotateZ(-math.Pi/3))
@@ -26,12 +26,12 @@ func main() {
 
 	cylinder := MakeClosedCylinder(Identity().Translate(1.8, 0, -0.5).Scale(0.7, 0.7, 0.7), cylinderMaterial, 0, 1)
 
-	sphere3Material := DefaultMaterial()
+	hexagonMaterial := DefaultMaterial()
 
-	sphere3Material.color = Color{1, 0.8, 0.1}
-	sphere3Material.specular = 1
-	sphere3Material.pattern = MakePerturbPattern(MakeStripePattern(Color{.7, .9, .5}, Color{.7, .7, .3}, Identity().Scale(.1, .1, .1).RotateZ(math.Pi/2).RotateY(math.Pi/4)))
-	sphere3 := MakeSphere(Identity().Translate(-1.5, 0.33, -0.75).Scale(0.33, 0.33, 0.33), sphere3Material)
+	hexagonMaterial.color = Color{1, 0.8, 0.1}
+	hexagonMaterial.specular = 1
+	hexagonMaterial.pattern = MakePerturbPattern(MakeStripePattern(Color{.7, .9, .5}, Color{.7, .7, .3}, Identity().Scale(.1, .1, .1).RotateZ(math.Pi/2).RotateY(math.Pi/4)))
+	hex := hexagon(Identity().Translate(-1.7, 0.125, 0).Scale(0.5, 0.5, 0.5), hexagonMaterial)
 
 	cubeMaterial := DefaultMaterial()
 
@@ -83,7 +83,34 @@ func main() {
 	lightColor := Color{1, 1, 1}
 	light := PointLight{lightPosition, lightColor}
 
-	world := World{light, []Object{sphere, cylinder, sphere3, cube, sphere5, floor, wall1, wall2, wall3}}
+	world := World{light, []Object{sphere, hex, sphere5, cylinder, cube, wall1, wall2, wall3, floor}}
 	c := camera.Render(world)
 	fmt.Printf(c.PPM())
+}
+
+func hexagonCorner(g *Group, material Material) Object {
+	return MakeSphereInGroup(Identity().Translate(0, 0, -1).Scale(.25, .25, .25), material, g)
+}
+
+func hexagonEdge(g *Group, material Material) Object {
+	transform := Identity().Translate(0, 0, -1).RotateY(-math.Pi/6).RotateZ(-math.Pi/2).Scale(0.25, 1, 0.25)
+	return MakeCylinderInGroup(transform, material, 0, 1, false, g)
+}
+
+func hexagonSide(g *Group, transform Matrix, material Material) *Group {
+	side := MakeGroupInGroup(transform, g)
+	hexagonCorner(side, material)
+	hexagonEdge(side, material)
+
+	return side
+}
+
+func hexagon(transform Matrix, material Material) Object {
+	hex := MakeGroup(transform)
+
+	for i := 0; i < 6; i++ {
+		hexagonSide(hex, Identity().RotateY(float64(i)*math.Pi/3), material)
+	}
+
+	return hex
 }

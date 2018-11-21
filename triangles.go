15895a55a7cd5caf5984b789a3457dfc984a7ef4
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-21 16:21:39 +0100

    Triangle uses generic Intersection and Normal

--- triangles.go
+++ triangles.go
@@ -5,8 +5,7 @@ import "math"
 type Triangle struct {
 	p1, p2, p3     Tuple
 	e1, e2, normal Tuple
-	material       Material
-	transform      Matrix
+	baseObject     BaseObject
 	parent         *Group
 }
 
@@ -14,7 +13,7 @@ func MakeTriangle(p1, p2, p3 Tuple, transofrm Matrix, material Material) Triangl
 	e1 := p2.Subtract(p1)
 	e2 := p3.Subtract(p1)
 	normal := e2.Cross(e1).Normalize()
-	return Triangle{p1: p1, p2: p2, p3: p3, e1: e1, e2: e2, normal: normal, transform: transofrm, material: material}
+	return Triangle{p1: p1, p2: p2, p3: p3, e1: e1, e2: e2, normal: normal, baseObject: BaseObject{transofrm, material}}
 }
 
 func (t Triangle) Parent() *Group {
@@ -22,40 +21,45 @@ func (t Triangle) Parent() *Group {
 }
 
 func (t Triangle) Transform() Matrix {
-	return t.transform
+	return t.baseObject.transform
 }
 
 func (t Triangle) NormalAt(p Tuple) Tuple {
-	worldNormal := t.Transform().Inverse().T().MultiplyByTuple(t.normal)
-	worldNormal.w = 0
-	return worldNormal
+	localNormalAt := func(p Tuple) Tuple {
+		return t.normal
+	}
+
+	return t.baseObject.NormalAt(p, localNormalAt)
 }
 
 func (t Triangle) Material() Material {
 
-	return t.material
+	return t.baseObject.material
 }
 
-func (t Triangle) Intersection(r Ray) (intersection []Intersection) {
-	transformedRay := r.Transform(t.Transform().Inverse())
-	intersection = make([]Intersection, 0)
+func (t Triangle) Intersection(r Ray) []Intersection {
+	localIntersect := func(transformedRay Ray) (intersections []Intersection) {
+		intersections = make([]Intersection, 0)
 
-	dirCrossE2 := transformedRay.direction.Cross(t.e2)
-	det := t.e1.Dot(dirCrossE2)
+		dirCrossE2 := transformedRay.direction.Cross(t.e2)
+		det := t.e1.Dot(dirCrossE2)
 
-	if math.Abs(det) > Epsilon {
-		f := 1 / det
-		p1ToOrigin := transformedRay.origin.Subtract(t.p1)
-		u := f * p1ToOrigin.Dot(dirCrossE2)
-		if u >= 0 && u <= 1 {
-			originCrossE1 := p1ToOrigin.Cross(t.e1)
-			v := f * transformedRay.direction.Dot(originCrossE1)
-			if v >= 0 && (u+v) <= 1 {
+		if math.Abs(det) > Epsilon {
+			f := 1 / det
+			p1ToOrigin := transformedRay.origin.Subtract(t.p1)
+			u := f * p1ToOrigin.Dot(dirCrossE2)
+			if u >= 0 && u <= 1 {
+				originCrossE1 := p1ToOrigin.Cross(t.e1)
+				v := f * transformedRay.direction.Dot(originCrossE1)
+				if v >= 0 && (u+v) <= 1 {
 
-				intersection = append(intersection, Intersection{t: f * t.e2.Dot(originCrossE1), object: t})
+					intersections = append(intersections, Intersection{t: f * t.e2.Dot(originCrossE1), object: t})
+				}
 			}
 		}
+
+		return
 	}
 
-	return
+	return t.baseObject.Intersection(r, localIntersect)
 }

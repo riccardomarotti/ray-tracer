0801255ae59e22ef3a449d00e785c42d9456da76
Riccardo Marotti <riccardo.marotti@gmail.com>
2018-11-22 10:55:45 +0100

    Add vertex parsing for obj

--- obj_file.go
+++ obj_file.go
@@ -2,13 +2,43 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 	"strings"
 )
 
-func ParseObjFile(input string) (output, warn, err string) {
-	lines := len(strings.Split(input, "\n"))
+type Obj struct {
+	vertices []Tuple
+}
 
-	warn = fmt.Sprintf("%d lines where discarded", lines)
+func ParseObjFile(input string) (output Obj, discardedLinesCount int, errors string) {
+	lines := strings.Split(input, "\n")
+	discardedLinesCount = 0
+
+	defer func() {
+		if err := recover(); err != nil {
+			errors = fmt.Sprintf("Fatal error during parsing: %s (%T)", err, err)
+		}
+	}()
+
+	if len(lines) <= 1 {
+		return
+	}
+
+	output.vertices = []Tuple{Tuple{}}
+
+	for _, line := range lines {
+		switch line[0] {
+		case 'v':
+			values := strings.Split(line, " ")
+			x, _ := strconv.ParseFloat(values[1], 64)
+			y, _ := strconv.ParseFloat(values[2], 64)
+			z, _ := strconv.ParseFloat(values[3], 64)
+			output.vertices = append(output.vertices, Point(x, y, z))
+
+		default:
+			discardedLinesCount++
+		}
+	}
 
 	return
 }
--- obj_file_test.go
+++ obj_file_test.go
@@ -2,14 +2,39 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"testing"
 )
+
+func TestEmptyInout(t *testing.T) {
+	input := ""
+
+	output, discardedLinesCount, err := ParseObjFile(input)
+
+	Assert(discardedLinesCount == 0, fmt.Sprintf("Discarded lines had to be 0, but where %d", discardedLinesCount), t)
+	Assert(err == "", err, t)
+	Assert(reflect.DeepEqual(Obj{}, output), fmt.Sprintf("output had to be empty, insted it was %v", output), t)
+}
 
 func TestIgnoringUnrecognizedLines(t *testing.T) {
 	input := "not parsable things\nwith two lines"
 
-	_, warn, _ := ParseObjFile(input)
+	_, discardedLinesCount, err := ParseObjFile(input)
 
-	expectedWaring := "2 lines where discarded"
-	Assert(warn == expectedWaring, fmt.Sprintf("\nExpected warning was: '%s'\nReceived: '%s'", expectedWaring, warn), t)
+	Assert(discardedLinesCount == 2, fmt.Sprintf("Discarded lines had to be 2, but where %d", discardedLinesCount), t)
+	Assert(err == "", "Errors occurred during parsing", t)
+}
+
+func TestVertexRecords(t *testing.T) {
+	input := `v -1 1 0
+v -1.0000 0.5000 0.0000
+v 1 0 0
+v 1 1 0`
+
+	output, discardedLinesCount, err := ParseObjFile(input)
+
+	Assert(discardedLinesCount == 0, fmt.Sprintf("Discarded lines had to be 0, but where %d", discardedLinesCount), t)
+	Assert(err == "", "Errors occurred during parsing", t)
+
+	AssertTupleEqual(Point(-1, 1, 0), output.vertices[1], t)
 }
